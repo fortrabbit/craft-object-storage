48761a72dbdc545eddb8650594128b470444452f
Oliver Stark <os@fortrabbit.com>
2020-05-26 19:45:15 +0200

    use the 'type' by default

--- src/config.example.php
+++ src/config.example.php
@@ -1,20 +1,24 @@
 <?php
+
 $objectStorageAccess = [
-    'url'      => "https://" . getenv('OBJECT_STORAGE_HOST'),
+    'url' => "https://" . getenv('OBJECT_STORAGE_HOST'),
     'endpoint' => "https://" . getenv('OBJECT_STORAGE_SERVER'),
-    'keyId'    => '$OBJECT_STORAGE_KEY',
-    'secret'   => '$OBJECT_STORAGE_SECRET',
-    'bucket'   => getenv('OBJECT_STORAGE_BUCKET'),
-    'region'   => getenv('OBJECT_STORAGE_REGION'),
+    'keyId' => '$OBJECT_STORAGE_KEY',
+    'secret' => '$OBJECT_STORAGE_SECRET',
+    'bucket' => getenv('OBJECT_STORAGE_BUCKET'),
+    'region' => getenv('OBJECT_STORAGE_REGION'),
+    'type' => 'fortrabbit\ObjectStorage\Volume',
 ];
 
 return [
     // Default volume
-    'objectStorageAssets' => array_merge($objectStorageAccess, [
-        'name'      => 'Object Storage Assets',
-        'subfolder' => 'assets',
-        'type'      => 'fortrabbit\ObjectStorage\Volume',
-    ]),
+    'objectStorageAssets' => array_merge(
+        $objectStorageAccess,
+        [
+            'name' => 'Object Storage Assets',
+            'subfolder' => 'assets'
+        ]
+    ),
 
     // Define more volumes like this:
     //
